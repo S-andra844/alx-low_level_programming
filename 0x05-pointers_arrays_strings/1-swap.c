571de23bf675e77914105cd50e8cb847d172d5fe
SandraNzile <williesandra.ws@gmail.com>
2023-03-28 04:01:57 -0700

    Swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,9 +3,9 @@
 /**
  * swap_int - take in two variable integers and swaps them
  *
- * @*a: swaps and stores address of b
+ * @a: swaps and stores address of b
  *
- * @*b: swaps and stores address of a
+ * @b: swaps and stores address of a
  *
  * Return: 0
  */
